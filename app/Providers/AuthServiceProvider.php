65f0ca5788d2cb814d8bb434e7ea6e0adfe2376a
yaolihui129 <yaolihui0506>
2019-12-12 21:01:24 +0800

    安装Laravel-admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,8 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
+        // $this->registerPolicies();
+		Schema::defaultStringLength(191);
         //
     }
 }
