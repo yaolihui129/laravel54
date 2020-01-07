7dd67f89b63b4b162f5a8c53e20e57095b4d4468
腰立辉 <yaolh@yonyou.com>
2020-01-07 18:39:19 +0800

    个人设置

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Model\User;
 class UserController extends Controller
 {
     //
@@ -12,12 +13,36 @@ class UserController extends Controller
 		
 		
 	}
-	
+        
+	//个人设置页面
 	public function setting(){
-		return view('user.setting');
+            $user= Auth::user();
+            return view('user.setting', compact('user'));
 	}
-	
-	public function settingStore(){
-		
+	//个人设置行为
+	public function settingStore(Request $request ){
+            //验证
+            $this->validate(request(), [
+                'name'=>'required|min:2',
+            ]);
+            //逻辑
+            $name= request('name');
+            $user = Auth::user();
+            //如果更改用户名
+            if($name!=$user->name){
+                if(User::where('name',$name)->count()>0){
+                    return back()->withErrors('用户名已经被注册');
+                }
+                $user->name = $name;
+            }
+            //如果有头像上传
+            if($request->file('avatar')){
+                $path = $request->file('avatar')->storePublicly($user->id);
+                $user->avatar = '/storage/'.$path;
+            }
+            $user->save();
+            //渲染
+            return back();
+            
 	}
 }
--- public/js/ylaravel.js
+++ public/js/ylaravel.js
@@ -1,10 +1,24 @@
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
 const editor = new wangEditor('content');
+if (editor.config){
+    editor.config.uploadImgUrl = '/posts/image/upload';
 
-editor.config.uploadImgUrl = '/posts/image/upload';
+    // 设置 headers（举例）
+    editor.config.uploadHeaders = {
+        'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+    };
 
-// 设置 headers（举例）
-editor.config.uploadHeaders = {
-    'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-};
+    editor.create();
+}
 
-editor.create();
+
+$(".preview_input").change(function(event){
+    var file = event.currentTarget.files[0];
+    var url = window.URL.createObjectURL(file);
+    $(event.target).next(".preview_img").attr("src", url);
+});
